a951f86d7764d98d23caa50ebe2827388f32968c
Alexey Paranyushkin <pak005@campus.mephi.ru>
2024-12-05 13:07:03 +0300

    added some more requests

--- Bot/backBot/requesting.py
+++ Bot/backBot/requesting.py
@@ -9,10 +9,45 @@ def set_adress(adr):
 adress = "http://localhost:8000/"  # ToDo: перед запуском удалять эту строку. Сделано для отладки
 
 
-def get_test_by_id(id):
+def GetTestById(id):  # Конкретный тест по его id
     r = requests.get(f"{adress}api/test/by-id", params={"id": str(id)}).json()
     if r.get("message") != 'success':
         raise KeyError("Test not found.")
     return r
 
-print(get_test_by_id(3))
+
+def GetTestByDiscipline(id):  # Возвращает список тестов заданой дисциплины
+    r = requests.get(f"{adress}api/test/by-discipline", params={"id": str(id)}).json()
+    if r.get("message") != 'success':
+        raise KeyError("Test not found.")
+    return r
+
+
+def GetTestByName(name):
+    r = requests.get(f"{adress}api/test/by-name", params={"name": str(name)}).json()
+    if r.get("message") != "success":
+        raise KeyError("Test not found.")
+    return r
+
+
+def GetStudentById(chatId):
+    r = requests.get(f"{adress}api/student/by-id", params={"id": str(chatId)}).json()
+    if r.get("message") != 'success':
+        raise KeyError("Student not found.")
+    return r
+
+
+def GetStudentGroupsById(chatId):
+    r = requests.get(f"{adress}api/student/by-id", params={"id": str(chatId)}).json()
+    if r.get("message") != 'success':
+        raise KeyError("Student not found.")
+    return r.get("data").get("subs")
+
+
+def GetResultsById(chatId):
+    r = requests.get(f"{adress}api/result/by-student", params={"id": str(chatId)}).json()
+    if r.get("message") != 'success':
+        raise KeyError("Student not found.")
+    return r.get("data")
+
+print(GetResultsById(408))
