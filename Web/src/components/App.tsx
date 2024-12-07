2ad29c9139005eeb653661dfeaf511ee5392a6d6
lenyakuchuk <Lenya.kuchuk@gmail.com>
2024-12-07 15:32:33 +0300

    add in main styles.css from mantine

--- Web/src/components/App.tsx
+++ Web/src/components/App.tsx
@@ -1,6 +1,3 @@
-import { MantineProvider } from '@mantine/core';
-import RegistrationComponent from "./RegistrationComponent.tsx";
-
 export default function App() {
-    return <MantineProvider><RegistrationComponent></RegistrationComponent></MantineProvider>;
+    return <></>
 }
--- Web/src/main.tsx
+++ Web/src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './components/App.tsx'
+import '@mantine/core/styles.css'
+import { createTheme, MantineProvider } from "@mantine/core";
+
+const theme = createTheme({
+});
+
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <MantineProvider theme={theme} defaultColorScheme={'dark'} >
+        <StrictMode>
+            <App />
+        </StrictMode>
+    </MantineProvider>
 )
