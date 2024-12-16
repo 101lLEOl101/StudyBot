437ec5499c3b105b5a1532e3b0cc69b92baab1bd
QQ-QQ-QQ-QQ <146654233+QQ-QQ-QQ-QQ@users.noreply.github.com>
2024-12-16 15:18:55 +0300

    Update entities.py

--- Bot/backBot/entities.py
+++ Bot/backBot/entities.py
@@ -101,3 +101,29 @@ class WholeTest:
         self.questions = questions
         self.results = results
         self.expiresAt = expiresAt
+
+class FrontDiscipline:
+    def __init__(self, name: str):
+        self.name = name 
+
+class FrontAnswer:
+    def __init__(self, text: str, studentText: str):
+        self.text = text
+        self.student_text = studentText
+
+class FrontQuestion:
+    def __init__(self, text: str, type: int, answers: list[Answer], buttons_text:list[str], is_answered:bool):
+        self.text = text
+        self.type = type
+        self.answers = answers
+        self.buttons_text = buttons_text
+        self.is_answered = is_answered
+
+class FrontTest:
+    def __init__(self, testName: str, start: datetime, end: datetime,
+                 questions: list[Question], discipline: Discipline):
+        self.testName = testName
+        self.start = start
+        self.end = end
+        self.questions = questions
+        self.discipline = discipline
