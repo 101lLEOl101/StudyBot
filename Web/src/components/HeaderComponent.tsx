de4819aad6d6456397ec9cd40ffd9be17e2c8923
Leo <lenya.kuchuk@gmail.com>
2025-05-29 19:12:33 +0300

    fix problem with logo in header

--- Web/src/components/HeaderComponent.tsx
+++ Web/src/components/HeaderComponent.tsx
@@ -7,6 +7,7 @@ import {
 import classes from '../styles/HeaderComponent.module.css';
 import {Link} from "react-router-dom";
 import {logout} from "../features/auth/authSlice.ts";
+import Logo from "../assets/LogoMephi.png"
 
 type HeaderComponentProps = {
     active_page: number;
@@ -20,7 +21,7 @@ export function HeaderComponent(props:HeaderComponentProps) {
         <Box pb={40}>
             <header className={classes.header}>
                 <Group justify="space-between" h="100%">
-                    <Image height={"80%"} radius="md" src="https://auth.mephi.ru/assets/new_logo-0d4e8ce16244ce25269fe731be496247e0c9d6ea475a0f517150f4fb12a53579.png" />
+                    <Image height={"80%"} radius="md" src={Logo} />
 
                     <Group h="100%" gap={0} visibleFrom="sm">
                         <Link to={"/active-tests"}  className={props.active_page === 0 ? classes.activeLink + ' ' + classes.link : classes.link}>
